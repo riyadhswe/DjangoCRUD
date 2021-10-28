4a07da50109e90c0dfa537602a1e46a5039ac072
riyadhswe <riyadhswe@gmail.com>
2021-10-28 13:23:25 +0600

    data create

--- App/urls.py
+++ App/urls.py
@@ -4,4 +4,5 @@ from App import views
 urlpatterns = [
     path('', views.index_views, name="index"),
     path('student/', views.student_views, name="student"),
+    path('student_info/<int:studentid>', views.studentinfo_views, name="student_info"),
 ]
--- App/views.py
+++ App/views.py
@@ -1,23 +1,35 @@
 from django.shortcuts import render
 from App import forms
+from App.models import student_model
 
 
 # Create your views here.
 def index_views(request):
-    diction = {'title': "This is index page"}
+    #For view data
+    student_list = student_model.objects.order_by('name')
+    #End View
+    diction = {'title': "This is index page",
+               'student_list': student_list}
     return render(request, 'index.html', context=diction)
 
 
 def student_views(request):
-    form = forms.student_form()
+    student_form = forms.student_form()
     # main logic for insert
     if request.method == "POST":
-        form = forms.student_form(request.POST)
+        student_form = forms.student_form(request.POST)
 
-        if form.is_valid():
-            form.save(commit=True)
+        if student_form.is_valid():
+            student_form.save(commit=True)
             return index_views(request)
         # End insert
 
-    diction = {'title': "This is Student page", 'student_form': form}
+    diction = {'title': "This is Student page", 'student_form': student_form}
     return render(request, 'student.html', context=diction)
+
+def studentinfo_views(request,studentid):
+    studentinfo = student_model.objects.get(pk=studentid)
+    diction = {'title': "This is StudentInformation page",
+               'studentinfo':studentinfo}
+    return render(request, 'student_info.html', context=diction)
+
